419cab00cbc2498464feb622cef2437a240887bf
arnajdas <arnajdas@in.ibm.com>
2020-12-09 16:10:53 +0530

    check for trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Checking for build trigger
+          Checking for build trigger again
         </p>
         <a
           className="App-link"
